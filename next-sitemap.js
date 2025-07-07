f2fc77e3a2e1e6c61c40e3b1d4ac70e44ce54efb
websonly <websonly1010@gmail.com>
2025-07-07 20:41:49 +0200

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,4 +2,6 @@
 module.exports = {
   siteUrl: 'https://cafeterasportatiles.online',
   generateRobotsTxt: true,
+  // Esto fuerza a que los archivos se escriban en la carpeta de exportación:
+  outDir: './out',
 }
